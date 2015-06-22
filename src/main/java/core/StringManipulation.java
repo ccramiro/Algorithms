24e0732801bc6f82c5e8914df97fb2343a53a131
Core Ramiro <coreramiro@gmail.com>
2015-06-22 19:47:18 +0200

    More algorithms to manipulate Strings

--- src/main/java/core/StringManipulation.java
+++ src/main/java/core/StringManipulation.java
@@ -1,9 +1,16 @@
 package core;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class StringManipulation {
+
+	// Private constructor - best way to avoid instantiation
+	// Class is final as side effect
+	private StringManipulation( ) { }
 
 	public static boolean getPalindrome( String iWord ){
 
@@ -21,6 +28,22 @@ public class StringManipulation {
 			}
 		}
 		return true;
+	}
+
+	public static Set<String> removeDuplicateWords( String iSentence ){
+		Set<String> lWords = new HashSet<>();
+		for ( String lWord : iSentence.split("\\W+") ) {
+			lWords.add( lWord );
+		}
+		return lWords;
+	}
+
+	public static Set<Character> getUsedLetters( String iSentence ){
+		Set<Character> iLetters = new TreeSet<>();
+		for ( char iLetter : iSentence.toCharArray() ) {
+			iLetters.add( iLetter );
+		}
+		return iLetters;
 	}
 
 }
