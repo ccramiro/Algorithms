91ba072f6873710cf7ff80033b81fcd4be5e7437
Core Ramiro <coreramiro@gmail.com>
2015-07-23 19:08:05 +0100

    Linked list: remove

--- src/main/java/list/LinkedList.java
+++ src/main/java/list/LinkedList.java
@@ -18,6 +18,7 @@ public class LinkedList {
 	private Node mHead;
 
 	public void insert( int iValue ){
+		// Case list empty or first element to be inserted
 		if ( null == mHead || mHead.mValue > iValue){
 			Node lNewNode = new Node( iValue );
 			lNewNode.mNext = mHead;
@@ -34,10 +35,44 @@ public class LinkedList {
 		lAuxNode.mNext = lNew;
 	}
 
+	public void remove( int iValue ){
+		if ( mHead == null ){
+			return;
+		}
+
+		// Case value to remove is head
+		if ( mHead.mValue == iValue ){
+			Node lAuxNode = mHead;
+			mHead = mHead.mNext;
+			lAuxNode = null;
+			return;
+		}
+
+		// Iterate until finding value
+		Node lAuxNode = mHead;
+		while ( lAuxNode.mNext != null && lAuxNode.mNext.mValue != iValue ){
+			lAuxNode = lAuxNode.mNext;
+		}
+
+		// Only remove if value was found in the list
+		if ( lAuxNode.mNext != null ){
+			Node lNextNode = lAuxNode.mNext;
+			lAuxNode.mNext = lNextNode.mNext;
+			lNextNode = null;
+		}
+
+	}
+
 	public LinkedList insertTest( int iValue ){
 		insert( iValue );
 		return this;
 	}
+
+	public LinkedList removeTest( int iValue ){
+		remove( iValue );
+		return this;
+	}
+
 	@Override
 	public String toString(){
 		StringBuffer lSB = new StringBuffer( "" );
@@ -58,7 +93,14 @@ public class LinkedList {
 		 .insertTest( 10 )
 		 .insertTest( 5 )
 		 .insertTest( 6 )
-		 .insertTest( 12 );
+		 .insertTest( 12 )
+		 .removeTest( 8 )
+		 .removeTest( 1 )
+		 .removeTest( 7 )
+		 .removeTest( 5 )
+		 .removeTest( 14 )
+		 .insertTest( 7 );
 		System.out.println( "List " + m );
 	}
+
 }
